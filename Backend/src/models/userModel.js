2262ac2f5fc962fc1f2976ea1ee4b44603bfaa94
mnshimiyimana <m.nshimiyim@alustudent.com>
2025-02-14 17:42:59 +0200

    Minor fixes

--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema(
     phoneNumber: { type: String, required: true, unique: true },
     userPreferences: [{ type: String }], 
     agency: { type: mongoose.Schema.Types.ObjectId, ref: "Agency", required: true },
-    // role: { type: String, enum: ["admin", "manager", "fuels"], required: true },
+    role: { type: String, enum: ["admin", "manager", "fuels"], required: true },
     role: { type: String, enum: ["admin", "manager"], required: true },
   },
   { timestamps: true }

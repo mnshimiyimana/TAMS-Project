c5de157d9947ee55cd968b74d99b6bb1561de873
mnshimiyimana <m.nshimiyim@alustudent.com>
2025-03-13 12:05:42 +0200

    Password setup

--- agency_system/next.config.ts
+++ agency_system/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/setup-password/:token',
+        destination: '/auth/setup-password/:token',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;

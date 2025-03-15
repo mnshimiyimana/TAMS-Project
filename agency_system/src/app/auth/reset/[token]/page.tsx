4991f0dcc65ed722133294c459a7fe1d4c9227f4
mnshimiyimana <m.nshimiyim@alustudent.com>
2025-03-15 22:10:09 +0200

    Rest fixed

--- agency_system/src/app/auth/reset/[token]/page.tsx
+++ agency_system/src/app/auth/reset/[token]/page.tsx
@@ -2,12 +2,6 @@
 import React from "react";
 import ResetPage from "../page";
 
-interface TokenPageProps {
-  params: {
-    token: string;
-  };
-}
-
-export default function ResetTokenPage({ params }: TokenPageProps): React.ReactElement {
+export default function ResetTokenPage(): React.ReactElement {
   return <ResetPage />;
 }
